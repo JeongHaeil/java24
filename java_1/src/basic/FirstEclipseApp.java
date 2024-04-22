2a72950adceb8a123af2907c6927e09555df92a7
itwill <itwill@DESKTOP-4Q2VRE0>
2024-04-22 16:38:22 +0900

    java_1

---
+++ java_1/src/basic/FirstEclipseApp.java
@@ -0,0 +1,12 @@
+package basic;
+
+//주석문 : 프로그램 설명
+//행주석(//설명),범위주석(/*설명*/),API주석(/** 설명)
+
+public class FirstEclipseApp {
+	public static void main(String[] args) {
+		System.out.print("이클립스로 처음 만든 프로그램입니다.");
+		System.out.println();
+		System.out.println("열심히 해봐요.");
+	}
+}
