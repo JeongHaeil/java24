81b22378817c2fb79a126cc78b41270c198229fe
itwill <itwill@DESKTOP-4Q2VRE0>
2024-05-08 12:00:00 +0900

    20240508

---
+++ java_2/src/xyz/itwill/enumerate/InterfaceOne.java
@@ -0,0 +1,11 @@
+package xyz.itwill.enumerate;
+
+public interface InterfaceOne {
+	//상수 필드 : 프로그램에서 값(리터럴) 대신 사용하기 위해 제공되는 단어
+	//=> 클래스에 상수필드를 작성할 수 있지만 인터페이스에 상수필드를 작성하여 사용하는것을 권장
+	
+	int INSERT=1, UPDATE=2, DELETE=3, SELLCT=4;//public static final 제한자 생략 가능
+	
+	
+	
+}
